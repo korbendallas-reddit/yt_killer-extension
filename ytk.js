6d0157395c0ffe84b34894337ed9b8ef61ffe759
korbendallas-reddit <reddit.korben.dallas@gmail.com>
2016-08-01 16:55:59 -0400

    Update ytk.js

--- ytk.js
+++ ytk.js
@@ -1,8 +1,10 @@
-var ytkMenu = '<div id="ytk_menu"><div class="ytk_menu_title">YouTube Killer<div id="ytk_menu_close"></div></div><div class="ytk_menu_sub_selector"><ul id="ytk_menu_sub_selector_list"></ul></div><div id="ytk_whitelist" class="ytk_menu_submit">White List</div><div id="ytk_blacklist" class="ytk_menu_submit">Black List</div></div>';
+var ytkMenu = '<div id="ytk_menu"><div class="ytk_menu_title"><div id="ytk_menu_close"></div>YouTube Killer</div><div class="ytk_menu_sub_selector"><ul id="ytk_menu_sub_selector_list"></ul></div><div id="ytk_whitelist" class="ytk_menu_submit">White List</div><div id="ytk_blacklist" class="ytk_menu_submit">Black List</div></div>';
 var userSubsLink = 'https://www.reddit.com/subreddits/mine/moderator.json';
 
 
 $('document').ready(function(){
+
+	addYtkHotSpots();
 
 	$('#siteTable').append(ytkMenu);
 
@@ -15,7 +17,8 @@ $('document').ready(function(){
 
 
 	$('#ytk_menu_close').off('click').on('click', function() {
-		$('#ytk_menu').hide();
+		$('#ytk_menu').css('top', 0);
+		$('#ytk_menu').css('left', -99999);
 	});
 
 	$('.ytk_menu_sub_selector_list li').off('click').on('click', function(event) {
@@ -23,11 +26,15 @@ $('document').ready(function(){
 	});
 
 	$('#ytk_whitelist').off('click').on('click', function() {
-		ytkSubmit(user, 'whitelist');
+		var submitParent = $(event.target).parent();
+
+		ytkSubmit(user, 'whitelist', $(submitParent).attr('thing'));
 	});
 
-	$('#ytk_blacklist').off('click').on('click', function() {
-		ytkSubmit(user, 'blacklist');
+	$('#ytk_blacklist').off('click').on('click', function(event) {
+		var submitParent = $(event.target).parent();
+		
+		ytkSubmit(user, 'blacklist', $(submitParent).attr('thing'));
 	});
 
 });
@@ -61,11 +68,10 @@ function generateYtkMenuItems() {
 
 				$.getJSON(modLink, function(jsonB) {
 
-					console.log(jsonB);
 					var modList = jsonB.data.children;
 
 					for (var j = 0; j < modList.length; j++) {
-						console.log(modList[j].name);
+
 						if (modList[j].name == 'yt_killer') {
 
 							ytkIsMod = true;
@@ -80,12 +86,12 @@ function generateYtkMenuItems() {
 			}
 
 			// Add the sub to the list
-			if (ytkIsMod) {
+			if (true) {
 
 				var thisSub = '<li id="' + subList[i].data.display_name + '" class="' + oddEven;
 
 				// Automatically select the current sub
-				if (thisSub == currentSub) {
+				if (subList[i].data.display_name  == currentSub) {
 
 					thisSub = thisSub + ' selected';
 
@@ -135,11 +141,11 @@ function generateYtkMenuItems() {
 		
 					if ($(selectedSub).hasClass('toggle')) {
 
-						var allSubs = $('.ytk_menu_sub_selector_list li');
+						var allSubs = $('#ytk_menu_sub_selector_list li');
 
 						for (var j = 0; j < allSubs.length; j++) {
 
-							allSubs[j].removeClass('selected');
+							$(allSubs[j]).removeClass('selected');
 		
 						}
 					}
@@ -151,11 +157,11 @@ function generateYtkMenuItems() {
 
 					if ($(selectedSub).hasClass('toggle')) {
 
-						var allSubs = $('.ytk_menu_sub_selector_list li');
+						var allSubs = $('#ytk_menu_sub_selector_list li');
 
 						for (var j = 0; j < allSubs.length; j++) {
 
-							allSubs[j].addClass('selected');
+							$(allSubs[j]).addClass('selected');
 
 						}
 					}
@@ -170,10 +176,18 @@ function generateYtkMenuItems() {
 }
 
 
-function ytkSubmit(user, state) {
 
-	var menuItems = $('#ytk_menu_sub_selector_list li');
+
+
+
+
+
+
+// Send to server
+function ytkSubmit(user, state, thing) {
+
 	var selectedSubs = '';
+	var menuItems = $('#ytk_menu_sub_selector_list li');
 
 	for (var i = 0; i < menuItems.length; i++) {
 
@@ -181,13 +195,13 @@ function ytkSubmit(user, state) {
 
 			if (!$(menuItems[i]).hasClass('toggle')) {
 
-				if (i == 0) {
+				if (selectedSubs.length > 0) {
 
 					selectedSubs = selectedSubs + ',';
 
 				}
 
-				selectedSubs = selectedSubs + menuItems[i].id;
+				selectedSubs = selectedSubs + $(menuItems[i]).attr('id');
 
 			} else {
 
@@ -204,12 +218,93 @@ function ytkSubmit(user, state) {
 
 	}
 
+	var submitUri = 'http://localhost/api.py?user=' + user + '&subs=' + selectedSubs + '&state=' + state + '&thing=' + thing;
+
+	$.getJSON(submitUri, function(json) {
+		alert(json);
+	});
+
 }
 
 
 
 
 
+// Add the icon next to links on the page (regular sub page)
+function addYtkHotSpots() {
 
+	// Handle comments differently than the sub
+	if (window.location.href.split('/').length > 7) {
 
+		addYtkHotSpotsInComments();
 
+		return;
+
+	}
+
+	var links = $('.thing.link p.title > a.title');
+
+	for (var i = 0; i < links.length; i++) {
+
+		if (links[i].innerHTML.indexOf('youtu.be') > -1 || links[i].innerHTML.indexOf('youtube.com') > -1) {
+
+			var thing = links[i].closest('.thing');
+			var thingId = $(thing).attr('id');
+			$('<img thing="' + thingId + '" class="ytk_hot_spot" src="http://www.underconsideration.com/brandnew/archives/youtube_ios_icon.png" height="25px" width="25px">').insertAfter($(links[i]));
+
+		}
+
+	}
+
+	ytkHotSpotHandler();
+
+}
+// Add the icon next to links on the page (comments section)
+function addYtkHotSpotsInComments() {
+
+	var links = $('.thing a');
+
+	for (var i = 0; i < links.length; i++) {
+	
+		if (links[i].innerHTML.indexOf('youtu.be') > -1 || links[i].innerHTML.indexOf('youtube.com') > -1) {
+
+			var thing = $(links[i]).closest('.thing');
+			var thingId = $(thing).attr('id');
+			$('<img thing="' + thingId + '" class="ytk_hot_spot" src="http://www.underconsideration.com/brandnew/archives/youtube_ios_icon.png" height="25px" width="25px">').insertAfter($(links[i]));
+
+		}
+
+	}
+
+	ytkHotSpotHandler();
+
+}
+// Handle click events
+function ytkHotSpotHandler() {
+
+	var hotSpots = $('.ytk_hot_spot');
+
+	for (var i = 0; i < hotSpots.length; i++) {
+
+		hotSpots[i].addEventListener("click", function(event) {
+			
+			var hotSpot = $(event.target);
+			var thingId = hotSpot.attr('thing');
+			$('#ytk_menu').attr('thing', thingId);
+			
+			var hotSpotLocation = hotSpot.offset();
+
+			$('#ytk_menu').css('top', hotSpotLocation.top);
+			$('#ytk_menu').css('left', hotSpotLocation.left);
+			
+			if (hotSpotLocation.right < 300) {
+
+				$('#ytk_menu').css('left', hotSpotLocation.left - 290);
+
+			}
+
+		});
+
+	}
+
+}
