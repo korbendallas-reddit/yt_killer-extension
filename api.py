5a8c8637c9b958cbabefc8b21c55ae4955edeb93
korbendallas-reddit <reddit.korben.dallas@gmail.com>
2016-08-10 08:05:11 -0400

    Update api.py

--- api.py
+++ api.py
@@ -10,15 +10,17 @@ def Main():
         keys = data.keys()
 
         if (len(keys) > 0):
-            
-            user = str(data['user'].value)
-            subs = str(data['subs'].value)
-            state = str(data['state'].value)
-            thing = str(data['thing'].value)
 
-            if user != 'user' and subs != 'subs' and state != 'state' and thing != 'thing':
+            mod = data['mod'].value
+            video = data['video'].value
+            thing = data['thing'].value
+            author = data['auth'].value
+            bl_wl = data['bl_wl'].value
+            subs = data['subs'].value
 
-                doStuff(user, subs, state, thing)
+            if mod != 'mod' and video != 'video' and thing != 'thing' and author != 'author' and bl_wl != 'bl_wl' and subs != 'subs':
+
+                doStuff(mod, video, thing, author, bl_wl, subs)
 
 
             return
@@ -26,37 +28,36 @@ def Main():
         
     except Exception as e:
         
-        print('HTTP/1.0 200 OK\n\n')
-        print('Content-Type: application/javascript')
-        print()
-        print('Error processing your request.')
+        print 'HTTP/1.0 200 OK\n\n'
+        print 'Content-Type: application/javascript'
+        print ''
+        print 'Error processing your request.' + e.message
         
         
     return
 
 
 # Handle data
-def doStuff(user, subs, state, thing):
+def doStuff(mod, video, thing, author, bl_wl, subs):
 
     try:
 
         # Validate data & make DB calls
         
-        print('HTTP/1.0 200 OK\n\n')
-        print('Content-Type: application/javascript')
-        print()
-        print('Yay! Something happened!')
+        print 'HTTP/1.0 200 OK\n\n'
+        print 'Content-Type: application/javascript'
+        print ''
+        print 'Request Processed'
 
     except Exception as e:
         
-        print('HTTP/1.0 200 OK\n\n')
-        print('Content-Type: application/javascript')
-        print()
-        print('Error processing your request.')
+        print 'HTTP/1.0 200 OK\n\n'
+        print 'Content-Type: application/javascript'
+        print ''
+        print 'Error processing your request.'
+
 
     return
-
-
 
 
 
